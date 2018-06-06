d7d9b90fe10d61e7e75f82c3a17f1ee1735d557f
LabberDev <emruocco2@gmail.com>
2018-06-06 16:45:09 +0200

    tests on invalidate chain

--- blockchain.test.js
+++ blockchain.test.js
@@ -3,11 +3,12 @@ const Block =   require('./block');
 
 describe('Blockchain', () => 
 {
-    let bc;
+    let bc, bc2;
 
     beforeEach(() =>
     {
         bc  =   new Blockchain();
+        bc2 =   new Blockchain();
     });  
 
     it ('starts with genesis block', () => 
@@ -23,4 +24,25 @@ describe('Blockchain', () =>
         //confronta i dati inseriti nel nuovo blocco, con il parametro passato
         expect(bc.chain[bc.chain.length-1].data).toEqual(data);
     });
+
+    it('validates a valid chain', () =>
+    {
+        bc2.addBlock('ccc');
+        expect(bc.isValidChain(bc2.chain)).toBe(true);
+    });
+
+    it('invalidates a chain with a corrupt genesis block', () =>
+    {
+        bc2.chain[0].data = 'Bad data';
+        expect(bc.isValidChain(bc2.chain)).toBe(false);
+    });
+    
+    it('invalidates a corrupt chain', () =>
+    {
+        bc2.addBlock('foo');
+        bc2.chain[1].data = 'not foo';
+        expect(bc.isValidChain(bc2.chain)).toBe(false);
+    });
+    
+
 });
