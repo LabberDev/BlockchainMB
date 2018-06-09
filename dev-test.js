d88027622af3dfef23d5ab7f0d39873919c25102
LabberDev <emruocco2@gmail.com>
2018-06-09 12:48:12 +0200

    test difficulty

--- dev-test.js
+++ dev-test.js
@@ -1,17 +1,28 @@
-const Block	=	require('./block');
+const Blockchain	=	require('./blockchain');
 
-runBlockchain(3);
+const bc			=	new Blockchain();
 
-function runBlockchain(numRipetizioni)
-{
-	const genesisBlock	=	Block.mineBlock(Block.genesis(), 'genesisBlock');
-	console.log(genesisBlock.toString());
+const ripetizioni	=	10;
 
-	var nBlock	=	genesisBlock;
+for (let i=0; i<ripetizioni; i++)
+	console.log(bc.addBlock(`numero ${i}`).toString());
 
-	for (var i = 0;	i < numRipetizioni;	i++)
+/*
+	const Block	=	require('./block');
+
+	runBlockchain(3);
+
+	function runBlockchain(numRipetizioni)
 	{
-		nBlock	=	Block.mineBlock(nBlock, i);
-		console.log(nBlock.toString());
+		const genesisBlock	=	Block.mineBlock(Block.genesis(), 'genesisBlock');
+		console.log(genesisBlock.toString());
+
+		var nBlock	=	genesisBlock;
+
+		for (var i = 0;	i < numRipetizioni;	i++)
+		{
+			nBlock	=	Block.mineBlock(nBlock, i);
+			console.log(nBlock.toString());
+		}
 	}
-}
+*/
