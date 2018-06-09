24d6d8515ed9af343d9f59abae58b1ead99c6d5f
LabberDev <emruocco2@gmail.com>
2018-06-09 18:12:37 +0200

    test transaction input

--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -24,6 +24,11 @@ describe('Transaction', () =>
             .toEqual(amount);
     });
 
+    it('inputs the balance of the wallet', () =>
+    {
+        expect(transaction.input.amount).toEqual(wallet.balance);
+    });
+
     describe('transacting with an amount that exceeds the balance', () =>
     {
         beforeEach(() =>
