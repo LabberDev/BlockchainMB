b300b7f64680c4abfe4cbee3328d4721a0bc21af
LabberDev <emruocco2@gmail.com>
2018-06-10 11:39:27 +0200

    test update transaction

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -6,7 +6,30 @@ class Transaction
     {
         this.id         =   ChainUtil.id();
         this.input      =   null;
+
+        //  n oggetti. Il primo è fatto dal saldo e dalla chiave pubblica del mittente
+        //  Dal secondo in poi possiedono il saldo da inviare e l'indirizzo del destinatario
         this.outputs    =   [];
+    }
+
+    update(senderWallet, recipient, amount)
+    {
+        //  restituisce un puntatore all'oggetto output, tale che output.address === senderWallet.publicKey
+        const senderOutput = this.outputs.find(output => output.address === senderWallet.publicKey);
+    
+        console.log(`_________1______SenderOutput: ${senderOutput} ______________`);
+
+        if (amount > senderOutput.amount)
+        {
+            console.log(`Amount: ${amount} exceeds balance.`);
+            return;
+        }
+    
+        senderOutput.amount = senderOutput.amount - amount;
+        this.outputs.push({ amount, address: recipient });
+        Transaction.signTransaction(this, senderWallet);
+    
+        return this;
     }
 
     static newTransaction(senderWallet, recipient, amount)
--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -29,6 +29,17 @@ describe('Transaction', () =>
         expect(transaction.input.amount).toEqual(wallet.balance);
     });
 
+    it('validates a valid transaction', () =>
+    {
+        expect(Transaction.verifyTransaction(transaction)).toBe(true);
+    });
+    
+    it('invalidates a corrupt transaction', () =>
+    {
+        transaction.outputs[0].amount = 50000;
+        expect(Transaction.verifyTransaction(transaction)).toBe(false);
+    });
+
     describe('transacting with an amount that exceeds the balance', () =>
     {
         beforeEach(() =>
@@ -42,4 +53,28 @@ describe('Transaction', () =>
             expect(transaction).toEqual(undefined);
         });
     });
+
+    describe('and updating a transaction', () =>
+    {
+        let nextAmount, nextRecipient;
+        beforeEach(() =>
+        {
+            nextAmount = 20;
+            nextRecipient = 'n3xt-4ddr355';
+            transaction = transaction.update(wallet, nextRecipient, nextAmount);
+        });
+      
+        it('subtracts the next amount from the sender’s output', () =>
+        {
+            expect(transaction.outputs.find(output => output.address === wallet.publicKey).amount)
+                .toEqual(wallet.balance - amount - nextAmount);
+        });
+      
+        it('outputs an amount for the next recipient', () =>
+        {
+            expect(transaction.outputs.find(output => output.address === nextRecipient).amount)
+                .toEqual(nextAmount);
+        });
+      });
+      
 });
