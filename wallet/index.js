5157832991850da5ae1796ee4440b5069d93a075
LabberDev <emruocco2@gmail.com>
2018-06-11 19:52:53 +0200

    Reward, Valid, and Clear

--- wallet/index.js
+++ wallet/index.js
@@ -51,7 +51,14 @@ class Wallet
         }
       
         return transaction;
-      }
+    }
+    
+    static blockchainWallet()
+    {
+        const blockchainWallet      =   new this();
+        blockchainWallet.address    =   'blockchain-wallet';
+        return blockchainWallet;
+    }
 }
 
 module.exports = Wallet;
--- wallet/transaction-pool.js
+++ wallet/transaction-pool.js
@@ -1,3 +1,5 @@
+const Transaction   =   require('../wallet/transaction');
+
 class TransactionPool
 {
     constructor()
@@ -25,6 +27,42 @@ class TransactionPool
     existingTransaction(address)
     {
         return this.transactions.find(transaction => transaction.input.address === address);
+    }
+
+    validTransactions()
+    {
+        return this.transactions.filter(transaction =>
+        {
+            //  reduce() somma tutti i return.
+            //  total è un accumulatore usato da reduce()
+            //  output fa riferimento ad ogni istanza di outputs
+            //  0 indica il valore di partenza di total 
+            const outputTotal   =   transaction.outputs.reduce((total, output) =>
+            {
+                return total + output.amount;
+            }, 0);
+
+            //  nelle transazioni vi sarà anche l'output con destinazione mittente
+            if (transaction.input.amount !== outputTotal)
+            {
+                console.log(`Invalid transaction from ${transaction.input.address}.`);
+                return;
+            }
+            
+            //  se non restituisce undefined
+            if (!Transaction.verifyTransaction(transaction))
+            {
+                console.log(`Invalid signature from ${transaction.input.address}.`);
+                return;
+            }
+
+            return transaction;
+        })
+    }
+
+    clear()
+    {
+        this.transactions   =   [];
     }
 }
   
